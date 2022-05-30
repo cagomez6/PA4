2749a29a525e214bc9d3b571794661e53293125b
Hao Wu <32400236+haowu84@users.noreply.github.com>
2022-05-30 00:36:49 -0700

    Create product.service.js

---
+++ react/src/services/product.service.js
@@ -0,0 +1,40 @@
+import http from "../http-common";
+
+class ProductDataService {
+    getAll() {
+        return http.get("/products");
+    }
+
+    get(id) {
+        return http.get(`/products/${id}`);
+    }
+
+    create(data) {
+        return http.post("/products", data);
+    }
+
+    update(id, data) {
+        return http.put(`/products/${id}`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/products/${id}`);
+    }
+
+    deleteAll() {
+        return http.delete(`/products`);
+    }
+
+    findByFilters(title, rating, min, max) {
+        return http.get(`/products`, {
+            params: {
+                title: title ? title : null,
+                rating: rating ? rating : null,
+                min: min ? min : null,
+                max: max ? max : null,
+            }
+        });
+    }
+}
+
+export default new ProductDataService();
